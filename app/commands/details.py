5ec14b85ce8d11992980b9221ee3c27d90c245f8
Matic Conradi <matic.conradi@me.com>
2022-11-04 12:31:48 +0100

    Bug fix

--- app/commands/details.py
+++ app/commands/details.py
@@ -129,5 +129,5 @@ class DetailsCommand(BaseCommand):
 		except CancelledError: pass
 		except Exception:
 			print(format_exc())
-			if environ["PRODUCTION"]: self.logging.report_exception(user=f"{ctx.author.id} {ctx.guild.id if ctx.guild is not None else -1}: /info {tickerId} venue:{venue}")
+			if environ["PRODUCTION"]: self.logging.report_exception(user=f"{ctx.author.id} {ctx.guild.id if ctx.guild is not None else -1}: /info {tickerId}")
 			await self.unknown_error(ctx)
