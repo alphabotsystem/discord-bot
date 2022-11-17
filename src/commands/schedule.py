bbef0a027f60ba3d4ea63e82b85fcb3ee88d9f12
Matic Conradi <matic.conradi@me.com>
2022-11-17 21:16:20 +0100

    Bug fix

--- src/commands/schedule.py
+++ src/commands/schedule.py
@@ -366,7 +366,7 @@ class ScheduleCommand(BaseCommand):
 					while timestamp < time(): timestamp += post["period"]
 					nextPost = datetime.fromtimestamp(timestamp, tz=utc).strftime("%d/%m/%Y %H:%M")
 					command = ' '.join([e for e in post['arguments'] if e != ""])
-					embed = Embed(title=f"Post a {post['command']} every {TIME_TO_PERIOD[post['period']]} starting at {nextPost} UTC.", description=f"Request: `{}`\nChannel: <#{post['channelId']}>\nScheduled by <@{post['authorId']}>", color=constants.colors["deep purple"])
+					embed = Embed(title=f"Post a {post['command']} every {TIME_TO_PERIOD[post['period']]} starting at {nextPost} UTC.", description=f"Request: `{command}`\nChannel: <#{post['channelId']}>\nScheduled by <@{post['authorId']}>", color=constants.colors["deep purple"])
 					await ctx.followup.send(embed=embed, view=DeleteView(database=self.database, pathId=f"details/scheduledPosts/{request.guildId}/{key}", userId=request.authorId), ephemeral=True)
 
 		except CancelledError: pass
