7433dca773092eb798c42fab890a15007ad934f7
Matic Conradi <matic.conradi@me.com>
2022-11-19 19:47:40 +0100

    Bug fix

--- src/assets/static_storage.py
+++ src/assets/static_storage.py
@@ -1,3 +1,3 @@
-icon_bw = "https://firebasestorage.googleapis.com/v0/b/nlc-bot-36685.appspot.com/o/alpha%2Fassets%2Ficon%2FIcon%20bw.png?alt=media"
-icon = "https://firebasestorage.googleapis.com/v0/b/nlc-bot-36685.appspot.com/o/alpha%2Fassets%2Ficon%2FIcon.png?alt=media"
-ichibot = "https://firebasestorage.googleapis.com/v0/b/nlc-bot-36685.appspot.com/o/alpha%2Fassets%2Ficon%2FIchibot.png?alt=media"
+icon_bw = "https://firebasestorage.googleapis.com/v0/b/nlc-bot-36685.appspot.com/o/alpha%2Fassets%2Ficon%2FIcon%20bw.png?alt=media&token=674416fe-1949-4743-8f15-2520bb8973b0"
+icon = "https://firebasestorage.googleapis.com/v0/b/nlc-bot-36685.appspot.com/o/alpha%2Fassets%2Ficon%2FIcon.png?alt=media&token=b3028a80-2443-4a88-8d3e-7db72fb1bd97"
+ichibot = "https://firebasestorage.googleapis.com/v0/b/nlc-bot-36685.appspot.com/o/alpha%2Fassets%2Ficon%2FIchibot.png?alt=media&token=43e86b1d-2e66-43e8-8a8a-0432e242a46e"
