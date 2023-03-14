8b02b3d2f14299c32b4db33505437cc59e98f965
Matic Conradi <matic.conradi@me.com>
2023-03-14 20:21:04 +0100

    Removed a licensed bot

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -543,10 +543,8 @@ elif botId == 0:
 elif botId == 1:
 	token = environ["HG60DTuv5GYWDUVX1ENONGdvEJ43_TOKEN"]
 elif botId == 2:
-	token = environ["QMREQIGRIWQZVMYKPQCU7C3EYBX1_TOKEN"]
-elif botId == 3:
 	token = environ["GN2CYWYS1SYR07BY4AKIBKVSEOO2_TOKEN"]
-elif botId == 4:
+elif botId == 3:
 	token = environ["N8V1MEBUJFSVP4IQMUXYYIEDFYI1_TOKEN"]
 
 bot.loop.run_until_complete(bot.start(token))
