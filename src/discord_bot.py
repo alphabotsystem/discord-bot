99606096b52d70f2927eaa42b5756df34f393831
Matic Conradi <matic.conradi@me.com>
2025-08-15 13:06:40 +0200

    Remove licensed bot

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -588,20 +588,18 @@ elif botId == 3:
 elif botId == 4:
 	token = environ["TOKEN_LYSQMRSJONMYQI8KSGXCMLO54IE2"]
 elif botId == 5:
-	token = environ["TOKEN_UIVTZSUV8YD74TLPRGQBIGTWNQG2"]
-elif botId == 6:
 	token = environ["TOKEN_26FIYWEEZNHCMSIGFI81BMBBFER2"]
-elif botId == 7:
+elif botId == 6:
 	token = environ["TOKEN_RWU79SZBNJUFMRPQBGJ3ZTNLMWA2"]
-elif botId == 8:
+elif botId == 7:
 	token = environ["TOKEN_QWMT0OT4G0TFBW5N27F6VGKHWQ82"]
-elif botId == 9:
+elif botId == 8:
 	token = environ["TOKEN_RUIPUKYXUASUOOGGCF0QYT4I1RN2"]
-elif botId == 10:
+elif botId == 9:
 	token = environ["TOKEN_8ZSFENTKEPNKDIAILE54MWNQNP62"]
-elif botId == 11:
+elif botId == 10:
 	token = environ["TOKEN_G27EPOPLSPWUAQALNJF7RPQPSHI2"]
-elif botId == 12:
+elif botId == 11:
 	token = environ["TOKEN_SU93NO2EQIZAEQEWP3LBSKIKAA43"]
 
 bot.loop.run_until_complete(bot.start(token))
