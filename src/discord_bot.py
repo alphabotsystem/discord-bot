38d931062d8936cd6023267dbf5d7988746106ed
Matic Conradi <matic.conradi@me.com>
2025-09-15 14:00:57 +0200

    Add new licensed bot 1/2

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -601,5 +601,7 @@ elif botId == 10:
 	token = environ["TOKEN_SU93NO2EQIZAEQEWP3LBSKIKAA43"]
 elif botId == 11:
 	token = environ["TOKEN_TDKVDVIDIHMGPDJNCWXEFFSFFGB2"]
+elif botId == 12:
+	token = environ["TOKEN_EBHBN39NEUBI8ULEHYP3RZT751C2"]
 
 bot.loop.run_until_complete(bot.start(token))
